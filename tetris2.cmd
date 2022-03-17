75535bbd77bbc6a0a7e3405f2682be9394f9566c
Muresan Razvan <67145369+MuresanRazv@users.noreply.github.com>
2022-03-18 01:13:04 +0200

    Add files via upload

---
+++ tetris2.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'tetris2.exe' with Python DLL being found.
+set PATH=c:\users\SENNNZ~1\ANACON~1;%PATH%
+set PYTHONHOME=c:\users\SENNNZ~1\ANACON~1
+"%~dp0.\tetris2.exe" %*
